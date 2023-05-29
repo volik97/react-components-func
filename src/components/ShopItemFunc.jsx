a6f863efac50f5fd60002ebeda6aa431af68549d
volik97 <volik97@inbox.ru>
2023-05-29 13:28:52 +0300

    first commit

---
+++ src/components/ShopItemFunc.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types'
+
+export const ShopItemFunc = ({item}) => {
+    const price = Math.round((item.price*100)/100).toFixed(2);
+  return (
+   <div className='main-content'>
+     <h2>{item.brand}</h2>
+     <h1>{item.title}</h1>
+     <h3>{item.description}</h3>
+     <div className="description">{item.descriptionFull}
+     </div>
+  <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+  <div className="divider"></div>
+  <div className="purchase-info">
+    <div className="price">{item.currency}{price}</div>
+    <button>Добавить в корзину</button>
+  </div>
+   </div>
+  )
+}
+
+ShopItemFunc.propTypes = {
+    item: PropTypes.shape({
+        brand: PropTypes.string,
+        title: PropTypes.string,
+        description: PropTypes.string,
+        descriptionFull: PropTypes.string,
+        price: PropTypes.number,
+        currency: PropTypes.string
+    })
+}
